d9b01fb3733170f75326e4e77f60f4b16400bb94
AdeshMask <adeshmaske255@gmail.com>
2022-03-17 19:13:58 +0530

    Added:Calculated length of x and y co-ordinate

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -3,6 +3,9 @@ package com.bridgelabz;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int x1 = 2;  int y1 = 3;
+        int x2 = 6;  int y2 = 4;
+        Integer length1 = (int)Math.sqrt((x2-x1)*2 + (y2-y1)*2);
+        System.out.println("Length of the line is : "+length1);
     }
 }
